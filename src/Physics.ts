48dbe67436bc0208245239a5153cede8a6256de5
Bhimana-Kamalesh <bhimanakls@gmail.com>
2026-02-13 15:43:56 +0530

    kkk

--- src/Physics.ts
+++ src/Physics.ts
@@ -10,22 +10,41 @@ export class Physics {
     }
 
     resolveCollisions(puck: Puck, mallets: Mallet[]) {
+        const goalWidth = this.width * 0.4;
+        const goalLeft = (this.width - goalWidth) / 2;
+        const goalRight = goalLeft + goalWidth;
+
         // Wall Collisions
+        // Left Wall
         if (puck.x - puck.radius < 0) {
             puck.x = puck.radius;
             puck.vx *= -1;
-            // TODO: Trigger Haptics/Sound
-        } else if (puck.x + puck.radius > this.width) {
+        }
+        // Right Wall
+        else if (puck.x + puck.radius > this.width) {
             puck.x = this.width - puck.radius;
             puck.vx *= -1;
         }
 
+        // Top Wall (Player 2 Goal)
         if (puck.y - puck.radius < 0) {
-            puck.y = puck.radius;
-            puck.vy *= -1;
-        } else if (puck.y + puck.radius > this.height) {
-            puck.y = this.height - puck.radius;
-            puck.vy *= -1;
+            // Check if within goal range
+            if (puck.x > goalLeft && puck.x < goalRight) {
+                // Let it pass (Goal scoring logic in Game.ts)
+            } else {
+                puck.y = puck.radius;
+                puck.vy *= -1;
+            }
+        }
+        // Bottom Wall (Player 1 Goal)
+        else if (puck.y + puck.radius > this.height) {
+            // Check if within goal range
+            if (puck.x > goalLeft && puck.x < goalRight) {
+                // Let it pass
+            } else {
+                puck.y = this.height - puck.radius;
+                puck.vy *= -1;
+            }
         }
 
         // Mallet Collisions
